622a115872b9e48f4ce259e74e9b89fefafd039d
kgrabek <kgrabek@bluesoft.net.pl>
2014-06-12 11:38:15 +0200

    page doesnt exist

--- assets/js/apps/contacts/show/show_controller.js
+++ assets/js/apps/contacts/show/show_controller.js
@@ -1,9 +1,17 @@
 ContactManager.module("ContactsApp.Show", function(Show, ContactManager, Backbone, Marionette, $, _){
   Show.Controller = {
-    showContact: function(model){
-      var contactView = new Show.Contact({
-        model: model
-      });
+    showContact: function(id){
+      var contacts = ContactManager.request("contact:entities");
+      var model = contacts.get(id);
+      var contactView;
+      if(model !== undefined){
+        contactView = new Show.Contact({
+          model: model
+        });
+      }
+      else{
+        contactView = new Show.MissingContact();
+      }
 
       ContactManager.mainRegion.show(contactView);
     }
